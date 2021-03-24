0d2a42412dcc57546f89418223ebac0fcbf153f5
19Dodo <dodobalosin@yahoo.com>
2021-03-24 04:08:13 +0200

    finnish

--- index.js
+++ index.js
@@ -1,58 +1,58 @@
-console.log("To do app");
 document.getElementById("add-button").addEventListener("click", function () {
 
-  fetch(`https://simple-json-server-scit.herokuapp.com/todo/dtruta`)
+    fetch(`https://simple-json-server-scit.herokuapp.com/todo/bDorin`)
 
-      .then((r) => r.json())
-      .then(saveData);
+        .then((r) => r.json())
+        .then(saveData);
 })
 
 function saveData(data){
-  const input = document.getElementById("input").value;
-  if (data.todo.length === 0){
-      postMethod(input);
-  } else {
-      putMethod(data.todo, input);
-  }
-  
+    const input = document.getElementById("input").value;
+    if (Object.keys(data).length === 0) {
+        postMethod(input);
+    } else {
+        putMethod(data.todo, input, false);
+    }
+    
 }
 
 function postMethod(value) {
-  const payload = {
-      id: "bDorin",
-      todo: [{
-          checked: false,
-          item: value,
-      }]
+    const payload = {
+        id: "bDorin",
+        todo: [{
+            checked: false,
+            item: value,
+        }]
 
-  }
+    }
 
-  fetch("https://simple-json-server-scit.herokuapp.com/todo", {
-      method: "POST",
-      headers: {
-          "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
-  }).then(getMethod);
+    fetch("https://simple-json-server-scit.herokuapp.com/todo", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+    }).then(getMethod);
 }
 
-function putMethod(oldData, newData) {
-  const payload = {
-      id: "bDorin",
-      todo: [{
-          checked: false,
-          item: newData
-      }]
+function putMethod(oldData, newData, checked) {
+    oldData.push({
+        checked: checked,
+        item: newData
+    })
 
-  }
-  
-  fetch(`https://simple-json-server-scit.herokuapp.com/todo/bDorin`, {
-      method: "PUT",
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(payload)
-  }).then(getMethod);
+    const payload = {
+        id: "bDorin",
+        todo: oldData
+    }
+    
+    fetch(`https://simple-json-server-scit.herokuapp.com/todo/bDorin`, {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    }).then(getMethod);
 }
 
 
@@ -60,44 +60,103 @@ getMethod();
 
 
 function getMethod() {
-  fetch(`https://simple-json-server-scit.herokuapp.com/todo/bDorin`)
+    fetch(`https://simple-json-server-scit.herokuapp.com/todo/bDorin`)
 
-      .then((r) => r.json())
-      .then(renderToDoList);
+        .then((r) => r.json())
+        .then(renderToDoList);
 
 }
 
 const todoListHtml = document.querySelector("#to-do-list");
 
 function renderToDoList(article) {
-  let todoList = article.todo;
-  todoListHtml.innerText = "";
-  for (const todo of todoList) {
-      let task = document.createElement("div");
-      task.setAttribute("class", "task");
+    if (Object.keys(article).length === 0) {
+        return;
+    }
 
-      todoListHtml.appendChild(task);
+    let todoList = article.todo;
+    todoListHtml.innerText = "";
 
-      let checkbox = document.createElement("input");
-      checkbox.setAttribute("type", "checkbox");
-      checkbox.setAttribute("class", "task-input-checkbox");
-      checkbox.checked = todo.checked;
+    for (let i = 0; i < todoList.length; i++) {
+        let task = document.createElement("div");
+        task.setAttribute("class", "task");
+        task.setAttribute("id", "id-" + i);
 
-      task.appendChild(checkbox);
+        todoListHtml.appendChild(task);
 
-      let taskText = document.createElement("div");
-      taskText.setAttribute("class", "task-text");
-      taskText.innerText = todo.item;
+        let checkbox = document.createElement("input");
+        checkbox.setAttribute("type", "checkbox");
+        checkbox.setAttribute("class", "task-input-checkbox");
+        checkbox.setAttribute("data-index", i);
+        checkbox.setAttribute("data-item", todoList[i].item);
+        checkbox.checked = todoList[i].checked;
+        checkbox.addEventListener("click", function (){
+            fetch(`https://simple-json-server-scit.herokuapp.com/todo/bDorin`)
+            .then((r) => r.json())
+            .then((data) => {
+                for(let i = 0; i < data.todo.length; i++){
+                    if (i == this.dataset.index){
+                        data.todo[i] = {
+                            checked: this.checked,
+                            item: this.dataset.item
+                        }
+                    }
+                }
+                
+                const payload = {
+                    id: "bDorin",
+                    todo: data.todo
+                }
+                
+                fetch(`https://simple-json-server-scit.herokuapp.com/todo/bDorin`, {
+                    method: "PUT",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(payload)
+                }).then(getMethod);
+            });
+        })
+
+        task.appendChild(checkbox);
+
+        let taskText = document.createElement("div");
+        taskText.setAttribute("class", "task-text");
+        taskText.innerText = todoList[i].item;
 
 
-      task.appendChild(taskText);
+        task.appendChild(taskText);
 
-      let trash = document.createElement("i");
-      trash.setAttribute("class", "fas fa-trash");
+        let trash = document.createElement("i");
+        trash.setAttribute("class", "fas fa-trash");
+        trash.setAttribute("data-index", i);
+        trash.setAttribute("data-item", todoList[i].item);
+        trash.addEventListener("click", function(){
+            fetch(`https://simple-json-server-scit.herokuapp.com/todo/bDorin`)
+            .then((r) => r.json())
+            .then((data) => {
+                for(let i = 0; i < data.todo.length; i++){
+                    if (i == this.dataset.index){
+                        data.todo.splice(i, 1);
+                        document.getElementById("id-"+i).remove();
+                    }
+                }
+                
+                const payload = {
+                    id: "bDorin",
+                    todo: data.todo
+                }
+                
+                fetch(`https://simple-json-server-scit.herokuapp.com/todo/bDorin`, {
+                    method: "PUT",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(payload)
+                }).then(getMethod);
+            });
+        });
 
-      task.appendChild(trash);
-  }
-
-
+        task.appendChild(trash);
+    }
 }
-
