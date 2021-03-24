e0f4715dbc692eca9f08b320f8a13c1b9124145d
19Dodo <dodobalosin@yahoo.com>
2021-03-24 04:16:17 +0200

    finnished

--- game.js
+++ game.js
@@ -1,81 +1,95 @@
 console.log("OOP Game");
 
-class GameObject{
-    constructor(){
-        this.width = 50;
-        this.height= 50;
-        this.x=0;
-        this.y=0;
-        this.generateRef();
-       // this.move(50, 225);
-        
-    }   
+class GameObject {
+  constructor() {
+    this.width = 50;
+    this.height = 50;
+    this.x = 0;
+    this.y = 0;
+    this.generateRef();
+    // this.move(50, 225);
+  }
+
+  generateRef() {
+    this.ref = document.createElement("div");
+    this.ref.style.width = `${this.width}px`;
+    this.ref.style.height = `${this.height}px`;
+    this.ref.style.position = "absolute";
+    this.ref.style.top = 0;
+    this.ref.style.left = 0;
+
+    document.getElementById("game-scene").appendChild(this.ref);
+  }
+
+  move(x, y) {
+    this.x = x;
+    this.y = y;
+    this.ref.style.transform = `translate(${this.x}px, ${this.y}px)`;
+  }
 }
 
-class Player{
-    constructor(){
-        this.width = 50;
-        this.height= 50;
-        this.x=0;
-        this.y=0;
-        this.generateRef();
-        this.move(50, 225);
-        
-    }   
-    generateRef(){
-        this.ref=document.createElement("div");
-        this.ref.style.width = `${this.width}px`;
-        this.ref.style.width = `${this.height}px`;
-        this.ref.style.background = "red" ;
-        this.ref.style.position ="absolute";
-        this.ref.style.top = 0;
-        this.ref.style.left = 0;
+class Player extends GameObject {
+  constructor() {
+    super();
+    this.ref.style.background = "blue";
+    this.move(50, 225);
+  }
 
-        document.getElementById("game-scene").appendChild(this.ref);
-    }
+  moveUp() {
+    this.move(this.x, this.y - 25);
+  }
 
-    move(x, y){
-        this.x = x;
-        this.y = y;
-        this.ref.style.transform = `translate(${this.x}px, ${this.y}px)`;
-    }
-
-    moveUp(){
-        this.move(this.x +25 , this.y);
-    }
-
-    moveDown(){
-        this.move(this.x , this.y +25);
-    }
+  moveDown() {
+    this.move(this.x, this.y + 25);
+  }
 }
-const player=new Player ();
+
+class Obstacle extends GameObject {
+  constructor() {
+    super();
+    this.ref.style.background = "red";
+    this.move(1060, 25);
+  }
+
+  moveLeft() {
+    this.move(this.x - 5, this.y);
+  }
+}
+
+/// --- User  input
 
 let keyUpPress = false;
 let keyDownPress = false;
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowUp") {
+    keyUpPress = true;
+  }
 
-document.addEventListener("keydown", (event)=>{
-    if(event.key === "ArrowUp"){
-        keyUpPress = true;
-    }
-    if(event.key=== "ArrowDowun"){
-        keyUpPress = true;
-    }
-})
+  if (event.key === "ArrowDown") {
+    keyDownPress = true;
+  }
+});
 
-document.addEventListener("keyup", (event) =>{
-    if(event.key === "ArrowUp"){
-        keyDownPress = false;
-    }
-    if(event.key=== "ArrowDown"){
-        keyDownPress = false;
-    }
-})
+document.addEventListener("keyup", (event) => {
+  if (event.key === "ArrowUp") {
+    keyUpPress = false;
+  }
 
+  if (event.key === "ArrowDown") {
+    keyDownPress = false;
+  }
+});
 
+/// --- User  input
 
-setInterval(()=>{
-    console.log(keyUpPress);
+const player = new Player();
+const obstacle = new Obstacle();
 
-    if(keyUpPress) player.moveUp();
-    if(keyDownPress) player.moveDown();
-},500);
+// Game Loop
+setInterval(() => {
+  console.log(keyUpPress);
+
+  if (keyUpPress) player.moveUp();
+  if (keyDownPress) player.moveDown();
+  obstacle.moveLeft();
+}, 250);
