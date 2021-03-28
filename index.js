1dc8b35e6e7b3625036adb9ac7d25d25dfb3ba58
19Dodo <dodobalosin@yahoo.com>
2021-03-28 19:47:36 +0200

    almost done

--- index.js
+++ index.js
@@ -1,46 +1,5 @@
-console.log("SpaceShip Generator");
-
-/*document.getElementById("generate-spaceship").addEventListener("click",()=>{
-    var img = ["1.png", "2.png", "3.png"];
-function imgRandom(imgArr) {
-   return imgArr[Math.floor(Math.random() * imgArr.length)];
-}
-console.log(imgRandom(img));
-}); */
-
-class Ship{
-    constructor(){
-        this.position= 0;
-        this.generateHtmlRef();
-    }
-
-    generateHtmlRef() {
-        this.ref = document.createElement("img");
-        this.ref.src = "1.png";
-        this.ref.classList.add("ship");
-        document.body.appendChild(this.ref);
-    }
-
-    move(x,y){
-        this.x = x;
-        this.y = y;
-        this.ref.style.transform = `translate(${this.x}px , ${this.y}px)`;
-    }
-    setShip() {
-        document.addEventListener("keydown", (event) => {
-          console.log(event);
-        this.moveCar(event.key);
-          
-        });
-      }
-    
-      moveShip(direction) {
-        if (direction === "ArrowRight") {
-          this.position += 5;
-          this.ref.style.marginLeft = `${this.position}px`;
-        } else if (direction === "ArrowLeft") {
-          this.position -= 5;
-          this.ref.style.marginLeft = `${this.position}px`;
-        }
-        
-}
+console.log("Spaceship Homework");
+let spaceship;
+document.getElementById("generate-spaceship").addEventListener("click", function() {
+    spaceship = new Spaceship();
+})
---
+++ spaceship.js
@@ -0,0 +1,80 @@
+console.log("Spaceship Generator");
+
+class Spaceship {
+    constructor() {
+    this.positionX = 0;
+    this.positionY = 0;
+    this.runningEngine = false;
+    this.generateHtmlRef();
+    this.setStartEngine();
+    this.setMoveSpaceship();
+    }
+
+    generateHtmlRef() {
+        let spaceshipArray = ["1.png", "2.png", "3.png"];
+        this.ref = document.createElement("img");
+        this.ref.style.position = "absolute";
+        this.ref.src = spaceshipArray[this.getRndInteger(0, 3)];
+        this.ref.setAttribute('width', '80px');
+        this.ref.setAttribute('height', '80px');
+        this.ref.classList.add("spaceships");
+        document.body.appendChild(document.createElement('br'));
+        document.body.appendChild(document.createElement('br'));
+        document.body.appendChild(document.createElement('br'));
+        document.body.appendChild(document.createElement('br'));
+        document.body.appendChild(document.createElement('br'));
+        document.body.appendChild(this.ref);
+    }
+
+    getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min) ) + min;
+    }
+    
+    setStartEngine() {
+        this.ref.addEventListener("click", () => {
+          this.startEngine();
+        });
+
+        this.ref.addEventListener("dblclick", () => {
+            this.runningEngine = false;
+        })
+
+    }
+
+      startEngine() {
+        if (!this.runningEngine) {
+          this.runningEngine = true;
+          console.log("Engine is starting");
+          this.ref.classList.add("spaceship--start");
+        }
+      }
+
+    setMoveSpaceship() {
+        document.addEventListener("keydown", (event) => {
+            if (this.runningEngine) {
+                this.moveSpaceship(event.key);
+            }
+        });
+    }
+
+    moveSpaceship(direction) {
+        console.log(this.positionX + ' : ' + this.positionX);
+        if (direction === "ArrowRight") {
+          this.positionX += 5;
+          this.ref.style.transform = `translate(${this.positionX}px, ${this.positionY}px)`;
+        } else if (direction === "ArrowLeft") {
+            this.positionX -= 5;
+            this.ref.style.transform = `translate(${this.positionX}px, ${this.positionY}px)`;
+        } else if (direction === "ArrowUp") {
+            this.positionY -= 5;
+          this.ref.style.transform = `translate(${this.positionX}px, ${this.positionY}px)`;
+          } else if (direction === "ArrowDown") {
+            this.positionY += 5;
+          this.ref.style.transform = `translate(${this.positionX}px, ${this.positionY}px)`;
+          }
+
+    }
+    
+
+
+}
