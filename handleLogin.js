9462ead4923a33b55c5421801a0356b76691abca
19Dodo <dodobalosin@yahoo.com>
2021-04-13 10:49:54 +0200

    finnished

---
+++ handleLogin.js
@@ -0,0 +1,18 @@
+const inputName = document.getElementById("name");
+const inputPassword = document.getElementById("password");
+
+const PASSWORD_CHECK = "123";
+
+document.getElementById("login").addEventListener("click", () => {
+  console.log("go");
+  const name = inputName.value;
+  const password = inputPassword.value;
+
+  if (name && password === PASSWORD_CHECK) {
+    document.cookie = `name=${name}`;
+    document.cookie = `password=${password}`;
+    localStorage.setItem("name", name);
+    localStorage.setItem("password", password);
+    window.location = "/";
+  }
+});
